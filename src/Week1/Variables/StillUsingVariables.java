a9aed3ff2712316ada164523e3672bc499f90877
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2019-10-11 21:34:05 -0700

    Solve "Still Using Variables" http://programmingbydoing.com/a/still-using-variables.html

--- src/Week1/Variables/StillUsingVariables.java
+++ src/Week1/Variables/StillUsingVariables.java
@@ -2,5 +2,8 @@ package Week1.Variables;
 
 class StillUsingVariables {
   public static void main(String[] args) {
+    String name = "Juan Valdez";
+    int year = 2010;
+    System.out.printf( "My name is %s and I'll graduate in %d.\n", name, year);
   }
 }
